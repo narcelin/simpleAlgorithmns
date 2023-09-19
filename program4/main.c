a0262dbc87ccc5ac2d39f348819593c066b0e1db
Nicolas Arcelin <arcelin.nicolas@gmail.com>
2023-09-18 21:18:37 -0400

    Initial Commit

---
+++ program4/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  program4
+//
+//  Created by Nicolas Arcelin on 9/18/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
